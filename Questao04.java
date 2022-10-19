cbc2c86a69e2785166e4322243fc2a9a9cf7c937
euslafelipe <felipeferraz2332@gmail.com>
2022-10-19 20:11:20 -0300

    Questao04

---
+++ Questao04.java
@@ -0,0 +1,22 @@
+package atividade2;
+
+import java.util.Scanner;
+
+public class Questao04 {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Digite sua Idade: ");
+        int idade = ler.nextInt();
+        if (idade >=18) {
+            System.out.println("voce tem idade para dirigir");
+            
+        }else{
+            System.out.println("voce nao tem idade para dirigir");
+        }
+
+        ler.close();
+}
+
+}
