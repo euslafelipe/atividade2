b78d766ca66e870ca373288dcd0647a9ada032b3
euslafelipe <felipeferraz2332@gmail.com>
2022-10-19 19:28:52 -0300

    Questao01

---
+++ Questao01.java
@@ -0,0 +1,30 @@
+package atividade2;
+
+import java.util.Scanner;
+
+public class Questao01 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+    
+        
+    System.out.println("Digite um numero:");
+        int num = ler.nextInt();
+        if (num < 0 ) {
+            System.out.println(num +" numero menor que zero ");
+            
+        }
+        if (num > 0) {
+            System.out.println(num+" numero maior que zero ");
+            
+        }
+        if (num == 0) {
+            System.out.println(num+" numero igual a zero ");
+        }
+ler.close();
+        
+
+    }
+}
+    
+    
+
