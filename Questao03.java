09b22ae306b25f488033b3d247f38f622d68d687
euslafelipe <felipeferraz2332@gmail.com>
2022-10-19 20:09:59 -0300

    Questao03

---
+++ Questao03.java
@@ -0,0 +1,22 @@
+package atividade2;
+
+import java.util.Scanner;
+
+public class Questao03 {
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Digite sua Idade: ");
+        int idade = ler.nextInt();
+        if (idade >=16) {
+            System.out.println("voce tem idade para votar");
+            
+        }else{
+            System.out.println("voce nao tem idade para votar");
+        }
+
+        ler.close();
+}
+
+}
